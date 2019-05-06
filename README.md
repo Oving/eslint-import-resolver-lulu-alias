ac94bffd3a234db51b4ae0d46b695961c5f81268
peishunqian108 <peishunqian108@lu.com>
2019-05-06 16:35:15 +0800

    feat: 读取lulu配置的能力

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# eslint-import-resolver-webpack-alias
-eslint import resolver for webpack alias
+# eslint-import-resolver-lulu-alias
+eslint import resolver for lulu(on webpack actually) alias
+
+## Installation
+
+Prerequisites: Node.js >=6.x and corresponding version of npm.
+
+```shell
+npm install eslint-import-resolver-lulu-alias --save-dev
+```
+
+## Usage
+
+Pass this resolver and its parameters to `eslint-plugin-import` using your `eslint` config file, `.eslintrc` or `.eslintrc.js`.
+
+```js
+"settings": {
+  "import/resolver": {
+    "lulu-alias": {
+      "configPath": "lulu.config.js"
+    }
+  }
+}
+```
+
+Note:
+
+- The alias config object contains one propertie: `configPath` 
+- The default value of `configPath` property is `lulu.config.js` if it is assigned to an undefined or not specified
--- index.js
+++ index.js
@@ -4,6 +4,16 @@ const path = require('path');
 
 const localResolve = module.constructor._findPath;
 const defaultExtension = ['.js', 'jsx', '.json'];
+const defaultWebpackConfig = {
+	externals: {},
+	resolve: {},
+	entry: {},
+	output: {},
+	module: {},
+	plugins: {},
+	externals: {},
+	resolve:{}
+};
 
 function findConfiguredPath(modulePath, alias) {
 	const __pathes = modulePath.split('/');
@@ -23,27 +33,33 @@ function fileWithExtension(path, extensions) {
 exports.interfaceVersion = 2;
 
 exports.resolve = function(modulePath, sourceFile, config) {
-	if (config && config.resolve && config.resolve.alias) {
-		const __pathes = modulePath.split('/');
+	try {
+    const luluConfig = require(path.join(process.cwd(), `${config.configPath || 'lulu.config.js'}`));
+		const { alias } = luluConfig.webpack(defaultWebpackConfig).resolve;
+    if (alias) {
+      const __pathes = modulePath.split('/');
+  
+      const aliasPath = findConfiguredPath(modulePath, alias);
+  
+      if (aliasPath) {
+        let filename = null;
 
-		const aliasPath = findConfiguredPath(modulePath, config.resolve.alias);
-
-		if (aliasPath) {
-			let filename = null;
-
-			if (fileWithExtension(aliasPath, defaultExtension)) {
-				filename = localResolve(aliasPath, []);
-			} else {
-				const filePath = aliasPath.startsWith('.')
-					? path.join(__dirname, aliasPath, ...__pathes.slice(1))
-					: path.join(aliasPath, ...__pathes.slice(1));
-				filename = localResolve(filePath, []);
-			}
-			return {
-				found: !!filename,
-				path: filename || null,
-			};
-		}
+				if (fileWithExtension(aliasPath, defaultExtension)) {
+					filename = localResolve(aliasPath, []);
+				} else {
+					const filePath = aliasPath.startsWith('.')
+						? path.join(__dirname, aliasPath, ...__pathes.slice(1))
+						: path.join(aliasPath, ...__pathes.slice(1));
+					filename = localResolve(filePath, []);
+				}
+				return {
+					found: !!filename,
+					path: filename || null,
+				};
+      }
+    }
+	} catch (err) {
+		console.log(err);
 	}
 	return { found: false };
 };
